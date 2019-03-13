2219fc1a9521594a687c6fd6903df03ae252db49
Wer <weronika.ak@wp.pl>
2019-03-13 20:30:31 +0100

    Add basic electron template

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    container
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  body {
+    background-color: #444;
+    color: #eee;
+    font-family: 'Lato', sans-serif;
+  }
+</style>
